4fa40d491520e7e1842050114968a9a3fa55b5d2
Pablo-gif-hub <pablodasif@gmail.com>
2026-03-15 19:39:40 -0400

    Analisis ia
    Analisis

--- app/ai_chat.py
+++ app/ai_chat.py
@@ -7,3 +7,16 @@ client = OpenAI(
     base_url="https://api.groq.com/openai/v1"
 )
 
+analisis_ia.html
+
+{% extends 'admin/master.html' %}
+
+{% block body %}
+
+<h2> Análisis Inteligente</h2>
+
+<div style="white-space: pre-line; border:1px solid #ccc; padding:15px;">
+    {{ analisis }}
+</div>
+
+{% endblock %}
